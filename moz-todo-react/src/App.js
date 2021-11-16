ec3cb6e030b2e3f8c02490c58758c9ff414828de
test <duwltjs5921@naver.com>
2021-11-16 13:57:01 +0000

    소스 완성 - 이후 기능추가예정

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -1,33 +1,105 @@
-import React from "react";
-import Form from "./components/Form";
-import FilterButton from "./components/FilterButton";
-import Todo from "./components/Todo";
+import React, { useState, useRef, useEffect } from 'react';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
+import Todo from './components/Todo';
+import { nanoid } from 'nanoid';
 
+function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+        ref.current = value;
+    });
+    return ref.current;
+}
 
 function App(props) {
-  const taskList = props.tasks.map(task => (<Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />));
+    const [tasks, setTasks] = useState(props.tasks);
+    const [filter, setFilter] = useState('All');
+    const FILTER_MAP = {
+        All: () => true,
+        Active: (task) => !task.completed,
+        Completed: (task) => task.completed,
+    };
+    const FILTER_NAMES = Object.keys(FILTER_MAP);
+    const filterList = FILTER_NAMES.map((name) => (
+        <FilterButton key={name} name={name} isPressed={name === filter} setFilter={setFilter} />
+    ));
 
-  return (
-    <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
-      <Form />
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
-      <h2 id="list-heading">
-        3 tasks remaining
-      </h2>
-      <ul
-        role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading"
-      >
-		  {taskList}
-      </ul>
-    </div>
-  );
+    const taskList = tasks
+        .filter(FILTER_MAP[filter]) //Array.prototype.filter()
+        .map((task) => (
+            <Todo
+                id={task.id}
+                name={task.name}
+                completed={task.completed}
+                key={task.id}
+                toggleTaskCompleted={toggleTaskCompleted}
+                deleteTask={deleteTask}
+                editTask={editTask}
+            />
+        ));
+    const tasksNoun = taskList.length > 1 ? 'tasks' : 'task';
+    const headingText = `${taskList.length} ${tasksNoun} remaining`;
+    const listHeadingRef = useRef(null);
+
+    function addTask(name) {
+        const newTask = { id: 'todo-' + nanoid(), name: name, completed: false };
+        //const newTask = { id: "id", name: name, completed: false };
+        setTasks([...tasks, newTask]);
+    }
+
+    function toggleTaskCompleted(id) {
+        const updatedTasks = tasks.map((task) => {
+            // if this task has the same ID as the edited task
+            if (id === task.id) {
+                // use object spread to make a new object
+                // whose `completed` prop has been inverted
+                return { ...task, completed: !task.completed };
+            }
+            return task;
+        });
+        setTasks(updatedTasks);
+    }
+
+    function deleteTask(id) {
+        const remainingTasks = tasks.filter((task) => id !== task.id);
+        setTasks(remainingTasks);
+    }
+
+    function editTask(id, newName) {
+        const editedTaskList = tasks.map((task) => {
+            if (id === task.id) {
+                return { ...task, name: newName };
+            }
+            return task;
+        });
+        setTasks(editedTaskList);
+    }
+
+    const prevTaskLength = usePrevious(tasks.length);
+    useEffect(() => {
+        if (tasks.length - prevTaskLength === -1) {
+            listHeadingRef.current.focus();
+        }
+    }, [tasks.length, prevTaskLength]);
+
+    return (
+        <div className="todoapp stack-large">
+            <h1>TodoMatic</h1>
+            <Form addTask={addTask} />
+            <div className="filters btn-group stack-exception">{filterList}</div>
+            <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+                {headingText}
+            </h2>
+            <ul
+                role="list"
+                className="todo-list stack-large stack-exception"
+                aria-labelledby="list-heading"
+            >
+                {taskList}
+            </ul>
+        </div>
+    );
 }
 
 export default App; // 다른 모듈에서 App컴포넌트들을 사용할 수 있도록 하는 명령
